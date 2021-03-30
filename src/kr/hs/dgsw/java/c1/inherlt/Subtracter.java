e47f457537f4deade99af8a6ea4f81db6d2792f1
tmdeh <yus174113@gmail.com>
2021-03-30 13:19:04 +0900

    3월 30일 3

--- src/kr/hs/dgsw/java/c1/inherlt/Subtracter.java
+++ src/kr/hs/dgsw/java/c1/inherlt/Subtracter.java
@@ -1,21 +1,19 @@
 package kr.hs.dgsw.java.c1.inherlt;
 
-public class Subtracter extends Adder{
-	
-	public Subtracter(int operand1, int operand2) {
-		super(operand1, operand2);
-	}
+public class Subtracter extends Adder {
 	
 	@Override
 	public int calculate() {
 		return operand1 - operand2;
 	}
+	
 	@Override
 	public String getOperator() {
-		// TODO Auto-generated method stub
 		return "-";
 	}
+	
 	public static void main(String[] args) {
-		
+		Subtracter subtractor = new Subtracter();
+		subtractor.execute();
 	}
 }
