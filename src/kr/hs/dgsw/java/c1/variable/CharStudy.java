b104822200d6bae0cb8d2d9953cb849086017676
tmdeh <66948129+tmdeh@users.noreply.github.com>
2021-04-27 09:43:25 +0900

    a

--- src/kr/hs/dgsw/java/c1/variable/CharStudy.java
+++ src/kr/hs/dgsw/java/c1/variable/CharStudy.java
@@ -1,22 +1,22 @@
-package kr.hs.dgsw.java.c1.variable;
-
-public class CharStudy {
-	public static void main(String[] args) {
-		char a = 65;
-		char ba = '∞°';
-		char bb = '»“';
-		
-		System.out.println(a);
-		//System.out.println(bb - ba);
-		
-//		for (char i = ''; i <= ''; i++) {
-//			System.out.println(i);
-//		}
-		int count = '∞°' - '»“' + 1;
-		System.out.println(count);
-		
-		char hanja = '∞°';
-		
-		System.out.println(hanja + 0);
-	}
-}
+//package kr.hs.dgsw.java.c1.variable;
+//
+//public class CharStudy {
+//	public static void main(String[] args) {
+//		char a = 65;
+//		char ba = 'a';
+//		char bb = 'b';
+//		
+//		System.out.println(a);
+//		//System.out.println();
+//		
+////		for (char i = ''; i <= ''; i++) {
+////			System.out.println(i);
+////		}
+//		int count = 'b' - 'a' + 1;
+//		System.out.println(count);
+//		
+//		char hanja = 'a';
+//		
+//		System.out.println(hanja + 0);
+//	}
+//}
---
+++ src/kr/hs/dgsw/java/c1/variable/test.java
@@ -0,0 +1,7 @@
+package kr.hs.dgsw.java.c1.variable;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
